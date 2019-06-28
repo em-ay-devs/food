a60179a9292fe42cd5de1490d371a6c6237decb9
mdang8 <mdang808@gmail.com>
2019-06-28 13:57:17 -0400

    FOOD-14 Fix recommendations method to avoid choosing duplicates

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -6,18 +6,15 @@ from src.lib.Recommend import Recommend
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
-
+    recommend = Recommend()
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
-    recommend = Recommend()
 
     @app.route('/')
     def index():
@@ -27,15 +24,11 @@ def create_app(test_config=None):
     def about():
         return 'TODO'
 
-    @app.route('/challenge', methods=['POST'])
-    def challenge():
-        challenge_str = request.json['challenge']
-        return challenge_str
-
     @app.route('/choose', methods=['GET', 'POST'])
     def choose():
         # Slack requires URL verification by returning a challenge string found in the request
         challenge_str = request.json['challenge'] if 'challenge' in request.json else ''
+        # gets the name of each recommendation object and puts them all in a list
         recommendations = [x['name'] for x in recommend.make_recommendations()]
         res_data = {
             'challenge': challenge_str,
