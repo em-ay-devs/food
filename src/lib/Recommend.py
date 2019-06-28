e3e187e8c828eb8d2635d53bafb94a6b03dfdff4
mdang8 <mdang808@gmail.com>
2019-06-28 13:57:17 -0400

    FOOD-14 Fix recommendations method to avoid choosing duplicates

--- src/lib/Recommend.py
+++ src/lib/Recommend.py
@@ -1,6 +1,7 @@
 import csv
 import os.path
-import random
+from random import randint
+from copy import copy
 
 CSV_FILE = os.path.abspath('src/configs/restaurant_data.csv')
 
@@ -34,9 +35,13 @@ class Recommend:
         return options
 
     def make_recommendations(self, num_choices=3):
+        # makes a shallow copy of the options member variable
+        remaining_options = copy(self.options)
         recommendations = []
         for x in range(num_choices):
-            chosen_option = self.options[random.randint(0, len(self.options))]
+            chosen_option = remaining_options[randint(0, len(remaining_options) - 1)]
             recommendations.append(chosen_option)
+            # removes the chosen option from the list so it won't be picked again on further iterations
+            remaining_options.remove(chosen_option)
         return recommendations
 
