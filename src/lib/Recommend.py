23f7bf329a2d59a57da128231b9ba72dee68a446
mdang8 <mdang808@gmail.com>
2019-06-28 14:16:32 -0400

    FOOD-14 Add challenge route back for Slack URL verification

--- src/lib/Recommend.py
+++ src/lib/Recommend.py
@@ -1,5 +1,5 @@
-import csv
 import os.path
+from csv import DictReader
 from random import randint
 from copy import copy
 
@@ -13,16 +13,18 @@ class Recommend:
     def __init__(self):
         self.options = self.read_csv()
 
+    # Reads the CSV file, extracts all of the food options, and puts them into a list of dictionary objects.
     @staticmethod
     def read_csv():
         options = []
         with open(CSV_FILE, mode='r') as csv_file:
-            reader = csv.DictReader(csv_file)
+            reader = DictReader(csv_file)
             line_count = 0
             for row in reader:
                 if line_count == 0:
                     line_count += 1
                 else:
+                    # dictionary object with the column headers in the CSV file
                     option = {
                         'name': row['Name'],
                         'takeout': row['Takeout'],
@@ -34,6 +36,8 @@ class Recommend:
                     line_count += 1
         return options
 
+    # Randomly selects a given number of recommendations for food places. By default, returns a list with 3
+    # recommendations.
     def make_recommendations(self, num_choices=3):
         # makes a shallow copy of the options member variable
         remaining_options = copy(self.options)
@@ -44,4 +48,3 @@ class Recommend:
             # removes the chosen option from the list so it won't be picked again on further iterations
             remaining_options.remove(chosen_option)
         return recommendations
-
